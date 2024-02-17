993e16445c98e06494d97c0c47901b193c157e9d
flick <github@0xflick.xyz>
2024-02-17 00:55:28 -0700

    checkpoint

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -14,29 +14,8 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const imgUrl = baseUrl.get() + "/api/meta/axolotl";
   return (
     <html lang="en">
-      <head>
-        <meta property="og:site_name" content="bitflick" />
-        <meta property="og:title" content="Bitflick Beta" />
-        <meta
-          property="og:description"
-          content="a new way to inscribe on bitcoin"
-        />
-        <meta property="og:image" content={imgUrl} />
-        <meta property="twitter:title" content="Axolotl Valley" />
-        <meta
-          property="twitter:description"
-          content="mint an axolotl on bitcoin"
-        />
-        <meta content="verification" name="LR1011" />
-        <meta property="twitter:image" content={imgUrl} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content="@0xflick" />
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content={imgUrl} />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- apps/www/src/app/page.tsx
+++ apps/www/src/app/page.tsx
@@ -1,5 +1,31 @@
 import { Home } from "@/routes/Home";
+import { baseUrl } from "@/utils/config";
 
 export default function Page() {
-  return <Home />;
+  const imgUrl = baseUrl.get() + "/api/meta/axolotl";
+  return (
+    <>
+      <head>
+        <meta property="og:site_name" content="bitflick" />
+        <meta property="og:title" content="Bitflick Beta" />
+        <meta
+          property="og:description"
+          content="a new way to inscribe on bitcoin"
+        />
+        <meta property="og:image" content={imgUrl} />
+        <meta property="twitter:title" content="Axolotl Valley" />
+        <meta
+          property="twitter:description"
+          content="mint an axolotl on bitcoin"
+        />
+        <meta content="verification" name="LR1011" />
+        <meta property="twitter:image" content={imgUrl} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@0xflick" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content={imgUrl} />
+      </head>
+      <Home />
+    </>
+  );
 }
